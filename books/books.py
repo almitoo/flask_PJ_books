bd0027afd63dda5d62afbf3b5fd16a7ea0e7b265
AmitIshay <109962844+AmitIshay@users.noreply.github.com>
2025-06-25 21:56:14 +0300

    add commit new flask

--- books/books.py
+++ books/books.py
@@ -5,27 +5,21 @@ from bson import ObjectId
 import datetime
 from .utilities_books import checkBookInBookList 
 from random import randint
-from random import randint
 books = Blueprint("books", __name__)
-
 # 🔹 יצירת ספר חדש
 @books.route("/createbook", methods=["POST"])
 @jwt_required()
 def create_book():
     data = request.json
     email = get_jwt_identity()
-
     user = users_collection.find_one({"email": email})
     if not user:
         return jsonify({"message": "User not found"}), 404
-
     title = data.get("title")
     author = data.get("author", user["full_name"])
     pages = data.get("pages", {})
-
     if not title or not pages:
         return jsonify({"message": "Missing title or pages"}), 400
-
     book = {
         "title": title,
         "author": author,
@@ -33,28 +27,23 @@ def create_book():
         "created_at": datetime.datetime.utcnow(),
         "num_pages": len(pages),
         "rating": 0,
+        "comments":[],
         "pages": pages
     }
-
     result = books_collection.insert_one(book)
-
     return jsonify({
         "message": "Book created successfully",
         "book_id": str(result.inserted_id)
     }), 201
-
 @books.route("/get_user_books", methods=["GET"])
 @jwt_required()
 def get_user_books():
     try:
         email = get_jwt_identity()
-
         user = users_collection.find_one({"email": email})
         if not user:
             return jsonify({"message": "User not found"}), 404
-
         user_books = books_collection.find({"user_id": user["_id"]})
-
         books_list = []
         for book in user_books:
             books_list.append({
@@ -64,18 +53,17 @@ def get_user_books():
                 "created_at": book.get("created_at").isoformat() if book.get("created_at") else None,
                 "num_pages": book.get("num_pages"),
                 "rating": book.get("rating"),
+                "comments":book.get("comments"),
                 "genre": book.get("genre"),
+                "sum_rating" : book.get("sum_rating") if book.get("sum_rating") else 0,
+                "counter_rating" : book.get("counter_rating") if book.get("counter_rating") else 0,
                 "pages": book.get("pages")
             })
 
         return jsonify({"books": books_list}), 200
-
     except Exception as e:
         print(f"Error in get_user_books: {e}")
         return jsonify({"message": "Internal server error"}), 500
-
-
-
 @books.route("/get_top_pick", methods=["GET"])
 def get_top_pick():
 #בוחר 4 ספרים רנדומליים מהדאטה בייס 
@@ -89,14 +77,17 @@ def get_top_pick():
                 "id": str(book["_id"]),
                 "title": book.get("title"),
                 "author": book.get("author"),
-                "created_at": book.get("created_at").isoformat(),
+                "created_at": book.get("created_at").isoformat() if book.get("created_at") else None,
                 "num_pages": book.get("num_pages"),
+                "rating": book.get("rating"),
+                "comments":book.get("comments"),
+                "genre": book.get("genre"),
+                "sum_rating" : book.get("sum_rating") if book.get("sum_rating") else 0,
+                "counter_rating" : book.get("counter_rating") if book.get("counter_rating") else 0,
                 "pages": book.get("pages")
             })
 
     return jsonify({"books": books_list}), 200
-
-
 @books.route("/get_top_rated", methods=["GET"])
 def get_top_rated():
     #בוחר 4 ספרים עם הדירוג הגבוה ביותר מהדאטה בייס 
@@ -105,21 +96,22 @@ def get_top_rated():
     for book in books:
         if (len(books_list)==4):
             break
-
         if (not checkBookInBookList(books_list , book)):
             books_list.append({
                 "id": str(book["_id"]),
                 "title": book.get("title"),
                 "author": book.get("author"),
-                "created_at": book.get("created_at").isoformat(),
+                "created_at": book.get("created_at").isoformat() if book.get("created_at") else None,
                 "num_pages": book.get("num_pages"),
+                "rating": book.get("rating"),
+                "comments":book.get("comments"),
+                "genre": book.get("genre"),
+                "sum_rating" : book.get("sum_rating") if book.get("sum_rating") else 0,
+                "counter_rating" : book.get("counter_rating") if book.get("counter_rating") else 0,
                 "pages": book.get("pages")
             })
 
     return jsonify({"books": books_list}), 200
-
-
-
 @books.route("/genre/<string:genre>" , methods=["GET"])
 def get_books_genre(genre):
     books = books_collection.find({"genre": genre})
@@ -130,13 +122,16 @@ def get_books_genre(genre):
                 "id": str(book["_id"]),
                 "title": book.get("title"),
                 "author": book.get("author"),
-                "created_at": book.get("created_at").isoformat(),
+                "created_at": book.get("created_at").isoformat() if book.get("created_at") else None,
                 "num_pages": book.get("num_pages"),
+                "rating": book.get("rating"),
+                "comments":book.get("comments"),
+                "genre": book.get("genre"),
+                "sum_rating" : book.get("sum_rating") if book.get("sum_rating") else 0,
+                "counter_rating" : book.get("counter_rating") if book.get("counter_rating") else 0,
                 "pages": book.get("pages")
             })
     return jsonify({"books": books_list}), 200
-
-
 @books.route("/recent_added" , methods=["GET"])
 def get_recent_added():
     books = books_collection.find({}).sort({"created_at":-1})
@@ -148,9 +143,152 @@ def get_recent_added():
                 "id": str(book["_id"]),
                 "title": book.get("title"),
                 "author": book.get("author"),
-                "created_at": book.get("created_at").isoformat(),
+                "created_at": book.get("created_at").isoformat() if book.get("created_at") else None,
                 "num_pages": book.get("num_pages"),
+                "rating": book.get("rating"),
+                "comments":book.get("comments"),
+                "genre": book.get("genre"),
+                "sum_rating" : book.get("sum_rating") if book.get("sum_rating") else 0,
+                "counter_rating" : book.get("counter_rating") if book.get("counter_rating") else 0,
                 "pages": book.get("pages")
             })
     return jsonify({"books": books_list}), 200
-
+@books.route("/new_rating_for_book/id=<string:id_book>" ,methods=["PUT"])
+def updateRatingOfBook(id_book):
+    id = ObjectId(id_book)
+    # חיפוש כל הספרים לפי user_id
+    bookObj = books_collection.find_one({"_id": id})
+    if not bookObj:
+        return jsonify({"message": "Book not found"}), 404
+    
+    data = request.json
+    rating = data.get("rating")
+    sum_rating = bookObj.get("sum_rating") if bookObj.get("sum_rating") else 0
+    counter_rating = bookObj.get("counter_rating") if bookObj.get("counter_rating") else 0
+    sum_rating+= rating
+    counter_rating+=1
+    rating  = sum_rating//counter_rating
+    #upadte in the DB
+    newvalues = { "$set": { 
+                    "sum_rating" :sum_rating,
+                    "counter_rating" :counter_rating,
+                    "rating" :rating
+     } }
+    
+    books_collection.update_one({"_id":id},newvalues)
+    
+    dic_print = { 
+                    "sum_rating" :sum_rating,
+                    "counter_rating" :counter_rating,
+                    "rating" :rating
+     } 
+    print(f"values update in the DB for book {id} {dic_print}")
+    return f"values update in the DB for book {id} {dic_print}", 200
+@books.route("/new_comment_for_book/id=<string:id_book>" ,methods=["PUT"])
+@jwt_required()
+def addNewCommentInBook(id_book):
+    id = ObjectId(id_book)
+    # חיפוש כל הספרים לפי user_id
+    bookObj = books_collection.find_one({"_id": id})
+    if not bookObj:
+        return jsonify({"message": "Book not found"}), 404
+    email = get_jwt_identity()
+    user = users_collection.find_one({"email": email})
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+    
+    data = request.json
+    user_name = user.get("full_name","null")
+    text_comment = data.get("comment","")
+    comments = list(bookObj.get("comments",[]))
+    comments.append({"user":user_name , "comment":text_comment})
+    #upadte in the DB
+    newvalues = { "$set": { 
+                    "comments" :comments,
+     } }
+    
+    books_collection.update_one({"_id":id},newvalues)
+    
+    dic_print = { 
+                    "comments" :comments
+     } 
+    print(f"values update in the DB for book {id} {dic_print}")
+    return f"values update in the DB for book {id} {dic_print}", 200
+@books.route("/newCommentAndRanking/id=<string:id_book>" ,methods=["PUT"])
+@jwt_required()
+def addRankingAndComment(id_book):
+    id = ObjectId(id_book)
+    # חיפוש כל הספרים לפי user_id
+    bookObj = books_collection.find_one({"_id": id})
+    if not bookObj:
+        return jsonify({"message": "Book not found"}), 404
+    email = get_jwt_identity()
+    user = users_collection.find_one({"email": email})
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+    
+    data = request.json
+    user_name = user.get("full_name","null")
+    text_comment = data.get("comment","")
+    rating = data.get("rating")
+    sum_rating = bookObj.get("sum_rating") if bookObj.get("sum_rating") else 0
+    counter_rating = bookObj.get("counter_rating") if bookObj.get("counter_rating") else 0
+    comments = list(bookObj.get("comments",[]))
+    comments.append({"user":user_name , "comment":text_comment})
+    sum_rating+= rating
+    counter_rating+=1
+    rating  = sum_rating//counter_rating
+    #upadte in the DB
+    newvalues = { "$set": { 
+                    "comments" :comments,
+                    "sum_rating" :sum_rating,
+                    "counter_rating" :counter_rating,
+                    "rating" :rating
+                    
+     } }
+    
+    books_collection.update_one({"_id":id},newvalues)
+    
+    dic_print = { 
+                    "comments" :len(comments),
+                    "sum_rating" :sum_rating,
+                    "counter_rating" :counter_rating,
+                    "rating" :rating
+     } 
+    print(f"values update in the DB for book {id} {dic_print}")
+    return f"values update in the DB for book {id} {dic_print}", 200
+@books.route("/delete/id=<string:id_book>" ,methods=["DELETE"])
+@jwt_required()
+def deleteBook(id_book):
+    id = ObjectId(id_book)
+    # חיפוש כל הספרים לפי user_id
+    bookObj = books_collection.find_one({"_id": id})
+    if not bookObj:
+        return jsonify({"message": "Book not found"}), 404
+    email = get_jwt_identity()
+    user = users_collection.find_one({"email": email})
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+    
+    books_collection.delete_one({"_id": id})
+    print(f"book has deleted from DB: id ={id}")
+    return f"book has been deleted ",200
+@books.route("/checkDeleteOption/id=<string:id_book>" ,methods=["GET"])
+@jwt_required()
+def checkDeleteBook(id_book):
+    id = ObjectId(id_book)
+    # חיפוש כל הספרים לפי user_id
+    bookObj = books_collection.find_one({"_id": id})
+    if not bookObj:
+        return jsonify({"message": "Book not found"}), 404
+    email = get_jwt_identity()
+    user = users_collection.find_one({"email": email})
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+    
+    userIdInBookObj = ObjectId(bookObj["user_id"])
+    print(f"{userIdInBookObj} == {user["_id"]}")
+    if  userIdInBookObj == user["_id"] :
+        return jsonify({"message": "Book can be deleted"}), 200
+    else:
+        return  jsonify({"message": "ERROR could not delete because the book was not made by the user "}), 400
